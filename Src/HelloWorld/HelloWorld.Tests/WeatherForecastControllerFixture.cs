fea0ad08777adce97e0180deeb60371fbb8ef9a5
Pramod Jangam <pjangam@equalexperts.com>
2020-01-08 14:48:46 +0530

    removed temporory added test case

--- Src/HelloWorld/HelloWorld.Tests/WeatherForecastControllerFixture.cs
+++ Src/HelloWorld/HelloWorld.Tests/WeatherForecastControllerFixture.cs
@@ -22,20 +22,5 @@ namespace HelloWorld.Tests
             //then
             forcastData.Should().HaveCount(5);
         }
-
-
-        [Fact]
-        public async Task TestDeserialization()
-        {
-            var str = "{\"price\":{\"amount\":15,\"currency\":{\"symbol\":\"$\",\"englishName\":\"United States dollar\",\"code\":\"USD\",\"number\":\"840\",\"namespace\":\"ISO-4217\",\"decimalDigits\":2,\"majorUnit\":1,\"minorUnit\":0.01,\"isObsolete\":false}},\"name\":\"abc\",\"shippingLocation\":\"Pune\"}";
-            var settings = new JsonSerializerSettings
-            {
-                NullValueHandling = NullValueHandling.Ignore,
-                MissingMemberHandling = MissingMemberHandling.Ignore
-            };
-            var x = JsonConvert.DeserializeObject<ProductDetail>(str, settings);
-            System.Console.WriteLine(x);
-
-        }
     }
 }
