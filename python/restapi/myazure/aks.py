36e781fd251f24e35f609771713df886bb7ee648
Pramod Jangam <pjangam@equalexperts.com>
2020-02-03 19:54:42 +0530

    working solution to create managed cluster as container service has been deprecated by azure

--- python/restapi/myazure/aks.py
+++ python/restapi/myazure/aks.py
@@ -17,36 +17,95 @@ class get_aks:
     def create(self, config):
         client_id = os.environ.get("AZURE_CLIENT_ID", None)
         secret = os.environ.get("AZURE_CLIENT_SECRET", None)
-        asyncCreate = self.cs_client.container_services.create_or_update(
+        # asyncCreate = self.cs_client.container_services.create_or_update(
+        #     config['resource_group'],
+        #     config['container_name'],
+        #     {
+        #         'location': config['location'],
+        #         "orchestrator_profile": {
+        #             "orchestrator_type": "Kubernetes"
+        #         },
+        #         "master_profile": {
+        #             "count": 1,
+        #             "dns_prefix": "MasterPrefixTest",
+        #             "vm_size": "Standard_D2s_v3"
+        #         },
+        #         "agent_pool_profiles": [{
+        #             "name": "agentpool0",
+        #             "count": 3,
+        #             "vm_size": "Standard_D2s_v3",
+        #             "dns_prefix": "AgentPrefixTest"  # - Optional in latest version
+        #         }],
+        #         "linux_profile": {
+        #             "admin_username": "acslinuxadmin",
+        #             "ssh": {
+        #                 "public_keys": [{
+        #                     "key_data": "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDWM58od7yvDBt+FYBr1ZPKIwKv50KR7SDDPA4nDqfxdLSwa1dDgHPnI0MuqJqJ1AECtgXHy+EbMf6yUJwPe8T3EZlNT43880FJfbcgalvP3AyxCmR332Oya1XW/8FtXB0+5vH//V0RdXV3NwWIkTfB8Ndkk6dEL8dXpaYqhoa8vam6mtLejxZ/qy19Cc0xgi0a5S2b4VJ3R4NNQuLsIqnLIxgHshAya9l9HejKbQXxReFlJgMHt/BcDiOjv8rPr98KDm5viKaT9v2ws+cAF/x3fnOvd8R41e5I7GaJfKhNtm4Dc0QJ7ip8l2DpJlyveeFqX0an69W7Ty3lvFulUIYz pramodjangam@Pramods-MacBook-Pro.local"
+        #                 }]
+        #             }
+        #         },
+        #         "servicePrincipalProfile": {
+        #             "secret": secret,
+        #             "clientId": client_id
+        #         }
+        #     }
+        # )
+
+        asyncCreate = self.cs_client.managed_clusters.create_or_update(
             config['resource_group'],
             config['container_name'],
             {
-                'location': config['location'],
-                "orchestrator_profile": {
-                    "orchestrator_type": "Kubernetes"
-                },
-                "master_profile": {
-                    "count": 1,
-                    "dns_prefix": "MasterPrefixTest",
-                    "vm_size": "Standard_D2s_v3"
-                },
-                "agent_pool_profiles": [{
-                    "name": "agentpool0",
-                    "count": 3,
-                    "vm_size": "Standard_D2s_v3",
-                    "dns_prefix": "AgentPrefixTest"  # - Optional in latest version
-                }],
-                "linux_profile": {
-                    "admin_username": "acslinuxadmin",
-                    "ssh": {
-                        "public_keys": [{
-                            "key_data": "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDWM58od7yvDBt+FYBr1ZPKIwKv50KR7SDDPA4nDqfxdLSwa1dDgHPnI0MuqJqJ1AECtgXHy+EbMf6yUJwPe8T3EZlNT43880FJfbcgalvP3AyxCmR332Oya1XW/8FtXB0+5vH//V0RdXV3NwWIkTfB8Ndkk6dEL8dXpaYqhoa8vam6mtLejxZ/qy19Cc0xgi0a5S2b4VJ3R4NNQuLsIqnLIxgHshAya9l9HejKbQXxReFlJgMHt/BcDiOjv8rPr98KDm5viKaT9v2ws+cAF/x3fnOvd8R41e5I7GaJfKhNtm4Dc0QJ7ip8l2DpJlyveeFqX0an69W7Ty3lvFulUIYz pramodjangam@Pramods-MacBook-Pro.local"
-                        }]
-                    }
-                },
-                "servicePrincipalProfile": {
-                    "secret": secret,
-                    "clientId": client_id
+                "location": config['location'],
+                # "tags": {
+                #     "tier": "production",
+                #     "archv2": ""
+                # },
+                "properties": {
+                    # "kubernetesVersion": "",
+                    "dnsPrefix": "dnsprefix1",
+                    "agentPoolProfiles": [
+                        {
+                            "name": "nodepool1",
+                            "count": 3,
+                            "vmSize": "Standard_D2s_v3",
+                            "osType": "Linux",
+                            "type": "VirtualMachineScaleSets",
+                            "availabilityZones": [
+                                "1",
+                                "2",
+                                "3"
+                            ],
+                            "enableNodePublicIP": False
+                        }
+                    ],
+                    "linuxProfile": {
+                        "adminUsername": "azureuser",
+                        "ssh": {
+                            "publicKeys": [
+                                {
+                                    "keyData": "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDWM58od7yvDBt+FYBr1ZPKIwKv50KR7SDDPA4nDqfxdLSwa1dDgHPnI0MuqJqJ1AECtgXHy+EbMf6yUJwPe8T3EZlNT43880FJfbcgalvP3AyxCmR332Oya1XW/8FtXB0+5vH//V0RdXV3NwWIkTfB8Ndkk6dEL8dXpaYqhoa8vam6mtLejxZ/qy19Cc0xgi0a5S2b4VJ3R4NNQuLsIqnLIxgHshAya9l9HejKbQXxReFlJgMHt/BcDiOjv8rPr98KDm5viKaT9v2ws+cAF/x3fnOvd8R41e5I7GaJfKhNtm4Dc0QJ7ip8l2DpJlyveeFqX0an69W7Ty3lvFulUIYz pramodjangam@Pramods-MacBook-Pro.local"
+                                }
+                            ]
+                        }
+                    },
+                    "networkProfile": {
+                        "loadBalancerSku": "standard",
+                        "outboundType": "loadBalancer",
+                        "loadBalancerProfile": {
+                            "managedOutboundIPs": {
+                                "count": 2
+                            }
+                        }
+                    },
+                    
+                    "servicePrincipalProfile": {
+                        "clientId": client_id,
+                        "secret": secret
+                    },
+                    "addonProfiles": {},
+                    "enableRBAC": True,
+                    "diskEncryptionSetID": "/subscriptions/subid1/resourceGroups/rg1/providers/Microsoft.Compute/diskEncryptionSets/des",
+                    "enablePodSecurityPolicy": False
                 }
             }
         )
